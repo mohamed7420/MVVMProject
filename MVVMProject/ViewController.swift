828c3e939a2594435807223e44ed87beb253c171
mohamedosama2074 <iosengks74@gmail.com>
2020-06-20 14:02:59 +0200

    Initial Commit

---
+++ MVVMProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMProject
+//
+//  Created by Mohamed on 6/20/20.
+//  Copyright © 2020 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
